60d7f1e20f84153c902361ce4c4667e182c4a392
AlexanderFaezov <s.faezov.com@mail.ru>
2024-04-15 16:50:00 +0500

    Добавьте файлы проекта.

---
+++ Person.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <string>
+
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+protected:
+	string Name;
+	int Age;
+
+public:
+
+	Person() { cout << "Активирован пустой конструктор базового класса: " << this << endl; };
+
+	Person(string name, int age)
+	{
+		cout << "Активирован конструктор с параметрами: " << this << endl;
+
+		setName(name);
+		setAge(age);
+	}
+
+	Person(Person& anotherPerson)
+	{
+		setName(anotherPerson.getName());
+		setAge(anotherPerson.getAge());
+	}
+
+	~Person() { cout << "Активирован деструктор базового класса: " << this << endl; };
+
+	void setName(string name) { Name = name; }
+
+	void setAge(int age) { Age = age; }
+
+	string getName() { return Name; }
+
+	int getAge() { return Age; }
+
+	Person& operator= (Person& anotherPerson);
+
+	friend istream& operator>>(istream& stream, Person& person);
+
+	friend ostream& operator<<(ostream& stream, Person& person);
+};
---
+++ Source.cpp
@@ -0,0 +1,31 @@
+﻿#include "Student.h"
+
+#include <iostream>
+
+Student& Student::operator=(Student& anotherStudent)
+{
+	Name = anotherStudent.Name;
+	Age = anotherStudent.Age;
+	SubjectName = anotherStudent.SubjectName;
+	Grade = anotherStudent.Grade;
+
+	return *this;
+}
+
+istream& operator>>(istream& stream, Student& student)
+{
+	cout << "Введите имя: "; getline(stream, student.Name);
+	cout << "Введите возраст: "; stream >> student.Age;
+
+	stream.ignore();
+
+	cout << "Введите предмет ученика: "; getline(stream, student.SubjectName);
+	cout << "Введите оценку ученика: "; stream >> student.Grade;
+
+	return stream;
+}
+
+ostream& operator<<(ostream& stream, Student& student)
+{
+	return { stream << "Имя: " << student.Name << "Возраст: " << student.Age << "Предмет: " << student.SubjectName << "Оценка: " << student.Grade };
+}
